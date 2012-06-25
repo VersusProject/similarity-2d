b266480e2e3b576d7051f5e14d39eca02f14100c
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-06-25 10:44:38 -0400

    Set categories.

--- src/main/java/gov/nist/itl/versus/similarity/comparisons/extract/impl/LabeledArrayFeatureExtractor.java
+++ src/main/java/gov/nist/itl/versus/similarity/comparisons/extract/impl/LabeledArrayFeatureExtractor.java
@@ -1,4 +1,3 @@
-
 /*
  * This software was developed at the National Institute of Standards and
  * Technology by employees of the Federal Government in the course of
@@ -19,66 +18,70 @@
  *    			Updated with abstract methods hasPreview() and hasPreviewName() to
  *               sync with updates to base class Extractor in versus-core.
  */
-
 package gov.nist.itl.versus.similarity.comparisons.extract.impl;
 
 import java.util.HashSet;
 import java.util.Set;
 
-
 import edu.illinois.ncsa.versus.UnsupportedTypeException;
 import edu.illinois.ncsa.versus.adapter.Adapter;
 import edu.illinois.ncsa.versus.descriptor.Descriptor;
 import edu.illinois.ncsa.versus.extract.Extractor;
+import edu.illinois.ncsa.versus.utility.HasCategory;
 import gov.nist.itl.versus.similarity.comparisons.adapter.HasLabels;
 import gov.nist.itl.versus.similarity.comparisons.adapter.impl.LabeledImageObjectAdapter;
 import gov.nist.itl.versus.similarity.comparisons.descriptor.impl.LabeledThreeDimensionalDoubleArrayFeature;
 
-public class LabeledArrayFeatureExtractor implements Extractor {
+public class LabeledArrayFeatureExtractor implements Extractor, HasCategory {
 
-	private LabeledThreeDimensionalDoubleArrayFeature extract(HasLabels adapter) {
-		return new LabeledThreeDimensionalDoubleArrayFeature(adapter.getLabels());
-	}
+    private LabeledThreeDimensionalDoubleArrayFeature extract(HasLabels adapter) {
+        return new LabeledThreeDimensionalDoubleArrayFeature(adapter.getLabels());
+    }
 
-	@Override
-	public LabeledImageObjectAdapter newAdapter() {
-		return new LabeledImageObjectAdapter();
-	}
+    @Override
+    public LabeledImageObjectAdapter newAdapter() {
+        return new LabeledImageObjectAdapter();
+    }
 
-	@Override
-	public String getName() {
-		return "Pixels2LabeledArray";
-	}
+    @Override
+    public String getName() {
+        return "Pixels2LabeledArray";
+    }
 
-	@Override
-	public Descriptor extract(Adapter adapter) throws UnsupportedTypeException {
-		if (adapter instanceof HasLabels) {
-			HasLabels hasLabels = (HasLabels) adapter;
-			return extract(hasLabels);
-		} else {
-			throw new UnsupportedTypeException();
-		}
-	}
+    @Override
+    public Descriptor extract(Adapter adapter) throws UnsupportedTypeException {
+        if (adapter instanceof HasLabels) {
+            HasLabels hasLabels = (HasLabels) adapter;
+            return extract(hasLabels);
+        } else {
+            throw new UnsupportedTypeException();
+        }
+    }
 
-	@Override
-	public Set<Class<? extends Adapter>> supportedAdapters() {
-		Set<Class<? extends Adapter>> adapters = new HashSet<Class<? extends Adapter>>();
-		adapters.add(HasLabels.class);
-		return adapters;
-	}
+    @Override
+    public Set<Class<? extends Adapter>> supportedAdapters() {
+        Set<Class<? extends Adapter>> adapters = new HashSet<Class<? extends Adapter>>();
+        adapters.add(HasLabels.class);
+        return adapters;
+    }
 
-	@Override
-	public Class<? extends Descriptor> getFeatureType() {
-		return LabeledThreeDimensionalDoubleArrayFeature.class;
-	}
-	
-	@Override
-	public boolean hasPreview() {
-		return false;
-	}
-	
-	@Override
-	public String previewName() {
-		return null;
-	}
+    @Override
+    public Class<? extends Descriptor> getFeatureType() {
+        return LabeledThreeDimensionalDoubleArrayFeature.class;
+    }
+
+    @Override
+    public boolean hasPreview() {
+        return false;
+    }
+
+    @Override
+    public String previewName() {
+        return null;
+    }
+
+    @Override
+    public String getCategory() {
+        return "2D";
+    }
 }
--- src/main/java/gov/nist/itl/versus/similarity/comparisons/measure/impl/RuzickaMeasure.java
+++ src/main/java/gov/nist/itl/versus/similarity/comparisons/measure/impl/RuzickaMeasure.java
@@ -14,128 +14,125 @@
  *  @version 1.2
  *    Date: Mon Jan 23 07:24:34 EST 2012
  */
-
-
 package gov.nist.itl.versus.similarity.comparisons.measure.impl;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import edu.illinois.ncsa.versus.UnsupportedTypeException;
 import edu.illinois.ncsa.versus.descriptor.Descriptor;
 import edu.illinois.ncsa.versus.descriptor.impl.GrayscaleHistogramDescriptor;
-import edu.illinois.ncsa.versus.descriptor.impl.PixelHistogramDescriptor;
 import edu.illinois.ncsa.versus.descriptor.impl.RGBHistogramDescriptor;
-import edu.illinois.ncsa.versus.descriptor.impl.ThreeDimensionalDoubleArrayFeature;
 import edu.illinois.ncsa.versus.measure.Measure;
 import edu.illinois.ncsa.versus.measure.Similarity;
 import edu.illinois.ncsa.versus.measure.SimilarityNumber;
 import edu.illinois.ncsa.versus.measure.SimilarityPercentage;
+import edu.illinois.ncsa.versus.utility.HasCategory;
 import gov.nist.itl.versus.similarity.comparisons.MathOps;
-import gov.nist.itl.versus.similarity.comparisons.descriptor.impl.LabeledThreeDimensionalDoubleArrayFeature;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Iterator;
+public class RuzickaMeasure implements Measure, HasCategory {
 
-public class RuzickaMeasure implements Measure
-{
-	private MathOps ops = new MathOps();
+    private MathOps ops = new MathOps();
 
-		@Override
-		public SimilarityPercentage normalize(Similarity similarity) {
-			return null;
-		}	
+    @Override
+    public SimilarityPercentage normalize(Similarity similarity) {
+        return null;
+    }
 
-        /**
-         * Compares two RGB Histograms.
-         *
-         * @param feature1 RGBHistogramDescriptor
-         * @param feature2 RGBHistogramDescriptor
-         * @return SimilarityNumber
-         * @throws Exception
-         */	
-		public SimilarityNumber compare(RGBHistogramDescriptor feature1, RGBHistogramDescriptor feature2) throws Exception 
-		{
-					// Check feature lengths, they must be equal
-					if( feature1.getNumBins() != feature2.getNumBins() ) {
-							throw new Exception("Features must have the same length");
-					}
-					
-					double[] normHist1 = ops.rgbNormHistogram2Double( feature1 );
-					double[] normHist2 = ops.rgbNormHistogram2Double( feature2 );
-					double measurement  = ops.d_ruzicka( normHist1, normHist2 );
-					return new SimilarityNumber(measurement);
-		}
-
-        /**
-         * Compares two Grayscale Histograms.
-         *
-         * @param feature1 GrayscaleHistogramDescriptor
-         * @param feature2 GrayscaleHistogramDescriptor
-         * @return SimilarityNumber
-         * @throws Exception
-         */
-        public SimilarityNumber compare(GrayscaleHistogramDescriptor feature1, GrayscaleHistogramDescriptor feature2) throws Exception {
-
-                // Check feature lengths, they must be equal
-                if( feature1.getNumBins() != feature2.getNumBins() ) {
-                        throw new Exception("Features must have the same length");
-                }
-
-                double[] normHist1 = ops.grayScaleNormHistogram2Double( feature1 );
-                double[] normHist2 = ops.grayScaleNormHistogram2Double( feature2 );
-                double measurement  = ops.d_ruzicka( normHist1, normHist2 );
-                return new SimilarityNumber(measurement);
+    /**
+     * Compares two RGB Histograms.
+     *
+     * @param feature1 RGBHistogramDescriptor
+     * @param feature2 RGBHistogramDescriptor
+     * @return SimilarityNumber
+     * @throws Exception
+     */
+    public SimilarityNumber compare(RGBHistogramDescriptor feature1, RGBHistogramDescriptor feature2) throws Exception {
+        // Check feature lengths, they must be equal
+        if (feature1.getNumBins() != feature2.getNumBins()) {
+            throw new Exception("Features must have the same length");
         }
-	
-		@Override
-		public SimilarityNumber compare(Descriptor feature1, Descriptor feature2)	throws Exception {
-				if (feature1 instanceof RGBHistogramDescriptor && feature2 instanceof RGBHistogramDescriptor) {
 
-						RGBHistogramDescriptor histogramFeature1 = (RGBHistogramDescriptor) feature1;
-						RGBHistogramDescriptor histogramFeature2 = (RGBHistogramDescriptor) feature2;
-						return compare(histogramFeature1, histogramFeature2);
-				}
-				else if( feature1 instanceof GrayscaleHistogramDescriptor && feature2 instanceof GrayscaleHistogramDescriptor) {
+        double[] normHist1 = ops.rgbNormHistogram2Double(feature1);
+        double[] normHist2 = ops.rgbNormHistogram2Double(feature2);
+        double measurement = ops.d_ruzicka(normHist1, normHist2);
+        return new SimilarityNumber(measurement);
+    }
 
-						GrayscaleHistogramDescriptor histogramFeature1 = (GrayscaleHistogramDescriptor) feature1;
-						GrayscaleHistogramDescriptor histogramFeature2 = (GrayscaleHistogramDescriptor) feature2;
-						return compare(histogramFeature1, histogramFeature2);
-				}
-				else {
-						throw new UnsupportedTypeException(
-										"Similarity measure expects features of type" + supportedTypesString() );
-				}
-		}	
+    /**
+     * Compares two Grayscale Histograms.
+     *
+     * @param feature1 GrayscaleHistogramDescriptor
+     * @param feature2 GrayscaleHistogramDescriptor
+     * @return SimilarityNumber
+     * @throws Exception
+     */
+    public SimilarityNumber compare(GrayscaleHistogramDescriptor feature1, GrayscaleHistogramDescriptor feature2) throws Exception {
 
-		private String supportedTypesString() {
-			String str="";
-			Set f   = supportedFeaturesTypes();
-			Iterator it = f.iterator();
-			while (it.hasNext()) {
-				str += ((Class)it.next()).getName() + " ";
-			}
-			return str;
-		}
+        // Check feature lengths, they must be equal
+        if (feature1.getNumBins() != feature2.getNumBins()) {
+            throw new Exception("Features must have the same length");
+        }
 
-		@Override
-		public Set<Class<? extends Descriptor>> supportedFeaturesTypes()
-		{
-			Set features = new HashSet();
-			features.add( RGBHistogramDescriptor.class );
-			features.add( GrayscaleHistogramDescriptor.class );
-			return features;
-		}
-	
-		public String getFeatureType() {
-			return RGBHistogramDescriptor.class.getName();
-		}
+        double[] normHist1 = ops.grayScaleNormHistogram2Double(feature1);
+        double[] normHist2 = ops.grayScaleNormHistogram2Double(feature2);
+        double measurement = ops.d_ruzicka(normHist1, normHist2);
+        return new SimilarityNumber(measurement);
+    }
 
-		@Override
-		public String getName() {
-			return "RuzickaMeasure";
-		}
+    @Override
+    public SimilarityNumber compare(Descriptor feature1, Descriptor feature2) throws Exception {
+        if (feature1 instanceof RGBHistogramDescriptor && feature2 instanceof RGBHistogramDescriptor) {
 
-		@Override
-		public Class getType() {
-			return RuzickaMeasure.class;
-		}	
+            RGBHistogramDescriptor histogramFeature1 = (RGBHistogramDescriptor) feature1;
+            RGBHistogramDescriptor histogramFeature2 = (RGBHistogramDescriptor) feature2;
+            return compare(histogramFeature1, histogramFeature2);
+        } else if (feature1 instanceof GrayscaleHistogramDescriptor && feature2 instanceof GrayscaleHistogramDescriptor) {
+
+            GrayscaleHistogramDescriptor histogramFeature1 = (GrayscaleHistogramDescriptor) feature1;
+            GrayscaleHistogramDescriptor histogramFeature2 = (GrayscaleHistogramDescriptor) feature2;
+            return compare(histogramFeature1, histogramFeature2);
+        } else {
+            throw new UnsupportedTypeException(
+                    "Similarity measure expects features of type" + supportedTypesString());
+        }
+    }
+
+    private String supportedTypesString() {
+        String str = "";
+        Set f = supportedFeaturesTypes();
+        Iterator it = f.iterator();
+        while (it.hasNext()) {
+            str += ((Class) it.next()).getName() + " ";
+        }
+        return str;
+    }
+
+    @Override
+    public Set<Class<? extends Descriptor>> supportedFeaturesTypes() {
+        Set features = new HashSet();
+        features.add(RGBHistogramDescriptor.class);
+        features.add(GrayscaleHistogramDescriptor.class);
+        return features;
+    }
+
+    public String getFeatureType() {
+        return RGBHistogramDescriptor.class.getName();
+    }
+
+    @Override
+    public String getName() {
+        return "Ruzicka";
+    }
+
+    @Override
+    public Class getType() {
+        return RuzickaMeasure.class;
+    }
+
+    @Override
+    public String getCategory() {
+        return "Intersection family";
+    }
 }
