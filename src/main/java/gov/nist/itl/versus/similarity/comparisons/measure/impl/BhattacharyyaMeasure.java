1376a1a534cdc572da154da7e6e7a63bbbec2e40
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-06-25 16:16:27 -0400

    Add some help.

--- src/main/java/gov/nist/itl/versus/similarity/comparisons/measure/impl/BhattacharyyaMeasure.java
+++ src/main/java/gov/nist/itl/versus/similarity/comparisons/measure/impl/BhattacharyyaMeasure.java
@@ -16,6 +16,7 @@
  */
 package gov.nist.itl.versus.similarity.comparisons.measure.impl;
 
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -29,9 +30,11 @@ import edu.illinois.ncsa.versus.measure.Similarity;
 import edu.illinois.ncsa.versus.measure.SimilarityNumber;
 import edu.illinois.ncsa.versus.measure.SimilarityPercentage;
 import edu.illinois.ncsa.versus.utility.HasCategory;
+import edu.illinois.ncsa.versus.utility.HasHelp;
+import edu.illinois.ncsa.versus.utility.HelpProvider;
 import gov.nist.itl.versus.similarity.comparisons.MathOps;
 
-public class BhattacharyyaMeasure implements Measure, HasCategory {
+public class BhattacharyyaMeasure implements Measure, HasCategory, HasHelp {
 
     private MathOps ops = new MathOps();
 
@@ -134,5 +137,15 @@ public class BhattacharyyaMeasure implements Measure, HasCategory {
     @Override
     public String getCategory() {
         return "Fidelity family or Squared-chord family";
+    }
+
+    @Override
+    public InputStream getHelpZipped() {
+        return HelpProvider.getHelpZipped(BhattacharyyaMeasure.class);
+    }
+
+    @Override
+    public String getHelpSHA1() {
+        return HelpProvider.getHelpSHA1(BhattacharyyaMeasure.class);
     }
 }
